cc521bf81e7a35aa86bd7ab6e71497695cf04b5c
Sahil Agarwal <agarwalsahil0210@gmail.com>
2020-09-17 11:12:16 +0530

    Winner declared

--- Day3/dices_app/lib/main.dart
+++ Day3/dices_app/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.redAccent,
       appBar: AppBar(
         centerTitle: true,
         title: Text('Dices App'),
@@ -27,28 +27,47 @@ class _DicePageState extends State<DicePage> {
   int leftdown = 2;
   int rightup = 3;
   int rightdown = 4;
-  void dice(){
-    setState(() {
-      leftup = Random().nextInt(6)+1;
-      leftdown = Random().nextInt(6)+1;
-      rightup = Random().nextInt(6)+1;
-      rightdown = Random().nextInt(6)+1;
-    });
+  String winner = "";
 
+  void dice() {
+    setState(() {
+      leftup = Random().nextInt(6) + 1;
+      leftdown = Random().nextInt(6) + 1;
+      rightup = Random().nextInt(6) + 1;
+      rightdown = Random().nextInt(6) + 1;
+      if (leftup > leftdown) {
+        if (leftup > rightup) {
+          if (leftup > rightdown)
+            winner = "left up";
+          else
+            winner = "rightdown";
+        } else if (rightup > rightdown) winner = "Right up";
+        else winner = "Right down";
+      }
+      else{
+        if(leftdown>rightup){
+          if(leftdown>rightdown) winner="Left Down";
+          else winner = "Right Down";
+        }
+        else if(rightup>rightdown) winner = "Right up";
+        else winner = "Right down";
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: <Widget>[
           SizedBox(
-            height: 80.0,
+            height: 60.0,
           ),
           Row(
             children: <Widget>[
               Expanded(
                 child: FlatButton(
-                  onPressed: (){
+                  onPressed: () {
                     dice();
                   },
                   child: Image.asset('images/dice$leftup.png'),
@@ -56,7 +75,7 @@ class _DicePageState extends State<DicePage> {
               ),
               Expanded(
                 child: FlatButton(
-                  onPressed: (){
+                  onPressed: () {
                     dice();
                   },
                   child: Image.asset('images/dice$rightup.png'),
@@ -65,13 +84,13 @@ class _DicePageState extends State<DicePage> {
             ],
           ),
           SizedBox(
-            height: 40.0,
+            height: 60.0,
           ),
           Row(
             children: <Widget>[
               Expanded(
                 child: FlatButton(
-                  onPressed: (){
+                  onPressed: () {
                     dice();
                   },
                   child: Image.asset('images/dice$leftdown.png'),
@@ -79,7 +98,7 @@ class _DicePageState extends State<DicePage> {
               ),
               Expanded(
                 child: FlatButton(
-                  onPressed: (){
+                  onPressed: () {
                     dice();
                   },
                   child: Image.asset('images/dice$rightdown.png'),
@@ -91,19 +110,14 @@ class _DicePageState extends State<DicePage> {
             height: 50.0,
           ),
           Text(
-            "Winner is Right Top",
+            'Winner is $winner !',
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0,
-              color: Colors.white
-            ),
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+                color: Colors.white),
           ),
         ],
       ),
     );
   }
 }
-
-
-
-
