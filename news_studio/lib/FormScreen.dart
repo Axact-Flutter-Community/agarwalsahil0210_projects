ac097853d8f6f72365843aa9ac06426c9e2cd80d
Sahil Agarwal <agarwalsahil0210@gmail.com>
2020-10-04 17:55:00 +0530

    modified form

--- news_studio/lib/FormScreen.dart
+++ news_studio/lib/FormScreen.dart
@@ -8,6 +8,73 @@ class FormScreen extends StatefulWidget {
 class _FormScreenState extends State<FormScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Form(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                  const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Your Full Name',
+                      labelStyle: TextStyle(color: Colors.orangeAccent),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular((25.0)),
+                        borderSide: BorderSide(color: Colors.orangeAccent),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                  const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'John@gmail.com',
+                        labelText: 'Email id',
+                        labelStyle: TextStyle(color: Colors.orangeAccent),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular((25.0)),
+                          borderSide: BorderSide(),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding:
+                  const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Phone Number',
+                        labelStyle: TextStyle(color: Colors.orangeAccent),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular((25.0)),
+                          borderSide: BorderSide(),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding:
+                  const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Gender',
+                        hintText: 'Male/Female',
+                        labelStyle: TextStyle(color: Colors.orangeAccent),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular((25.0)),
+                          borderSide: BorderSide(),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
